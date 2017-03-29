a93579d123cbd32e3f540c1babbf75d9e92fcf6b
AlKapkone <kapko2311@gmail.com>
2017-03-29 15:15:09 +0300

    Initial commit

---
+++ src/com/orest/kapko/vakoms/project/finished/User.java
@@ -0,0 +1,15 @@
+package com.orest.kapko.vakoms.project.finished;
+
+import java.util.ArrayList;
+
+public class User {
+    int id;
+    String name, email;
+
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+}
---
+++ src/com/orest/kapko/vakoms/project/finished/sw10.java
@@ -0,0 +1,213 @@
+package com.orest.kapko.vakoms.project.finished;
+
+import java.util.Enumeration;
+import java.util.Vector;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+public class sw10 extends JFrame {
+    /** Creates new form sw10 */
+    DefaultTableModel dtm;
+    public sw10() {
+        initComponents();
+        jTable1.setModel(dtm = new DefaultTableModel());
+        dtm.addColumn("Ім'я");
+        dtm.addColumn("Email");
+    }
+
+    /** This method is called from within the constructor to
+     * initialize the form.
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+                new Object [][] {
+                        {null, null, null, null},
+                        {null, null, null, null},
+                        {null, null, null, null},
+                        {null, null, null, null}
+                },
+                new String [] {
+                        "Title 1", "Title 2", "Title 3", "Title 4"
+                }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        jButton1.setText("Add user");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Delete user");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setText("Update user");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        jButton4.setText("Search user");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
+        JLabel jLabel1 = new JLabel("Список працівників", SwingConstants.RIGHT);
+        jLabel1.setFont(new java.awt.Font("DejaVu Sans", 1, 28)); // NOI18N
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addContainerGap()
+                                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 404, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addGap(172, 172, 172)
+                                                .addComponent(jLabel1))
+                                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                                .addContainerGap()
+                                                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 184, Short.MAX_VALUE)
+                                                .addGap(18, 18, 18)
+                                                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 203, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addGap(90, 90, 90)
+                                                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 238, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addGap(90, 90, 90)
+                                                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 235, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGap(28, 28, 28)
+                                .addComponent(jLabel1)
+                                .addGap(18, 18, 18)
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(43, 43, 43)
+                                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(jButton1)
+                                        .addComponent(jButton2))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jButton3)
+                                .addContainerGap(20, Short.MAX_VALUE))
+        );
+
+        pack();
+    }
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
+        JTextField xField = new JTextField(15);
+        JTextField yField = new JTextField(15);
+
+        JPanel myPanel = new JPanel();
+        myPanel.add(new JLabel("Name:"));
+        myPanel.add(xField);
+        myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+        myPanel.add(new JLabel("Email:"));
+        myPanel.add(yField);
+
+        int result = JOptionPane.showConfirmDialog(null, myPanel,
+                "Please Enter New User's information", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            System.out.println("The name is: " + xField.getText());
+            System.out.println("The email is: " + yField.getText());
+        }
+
+        Object row[]=new Object[2];
+        row[0]= xField.getText();
+        row[1]= yField.getText();
+        dtm.addRow(row);
+    }
+
+//    private void jButton1ActionPerformed5(java.awt.event.ActionEvent evt) {
+//        Object row[]=new Object[2];
+//        while((row[0]=JOptionPane.showInputDialog("Enter the name:")).equals(""));
+//        while((row[1]=JOptionPane.showInputDialog("Enter email:")).equals(""));
+//        dtm.addRow(row);
+//    }
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {
+        // TODO add your handling code here:
+        int t[]=jTable1.getSelectedRows();
+        if(t.length>1)
+            JOptionPane.showMessageDialog(null, "Please select only one row");
+        else if(t.length==0)
+            JOptionPane.showMessageDialog(null,"Please select a row");
+        else
+            dtm.removeRow(t[0]);
+    }
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {
+        // TODO add your handling code here:
+        int t[]=jTable1.getSelectedRows();
+        if(t.length==0)
+            JOptionPane.showMessageDialog(null,"Please select a row");
+        else
+        {
+            for(int i=t.length-1;i >= 0;i--)
+                dtm.removeRow(t[i]);
+        }
+    }
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {
+        // TODO add your handling code here:
+        int count=0;
+        String s = JOptionPane.showInputDialog("Enter the name:");
+        Enumeration rows=dtm.getDataVector().elements();
+        while(rows.hasMoreElements())
+        {
+            if(((Vector)rows.nextElement()).elementAt(0).equals(s))
+            {
+                jTable1.getSelectionModel().setSelectionInterval(count,count);
+            }
+            count++;
+        }
+    }
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new sw10().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    // End of variables declaration
+
+}
